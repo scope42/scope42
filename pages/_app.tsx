b62f2dc57a4ff799c9bb0cba5ffddaf998f5543b
Erik Hofer <erik-hofer@mail.de>
2021-06-19 15:52:35 +0200

    Add basic layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import 'antd/dist/antd.css';
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "antd/dist/antd.css"
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { Layout, Menu } from "antd"
+import Link from 'next/link'
+import { useRouter } from "next/router"
+
+const { Header, Content, Footer } = Layout
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  return (
+    <Layout className="layout" style={{ minHeight: '100%' }}>
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[router.pathname]}>
+          <Menu.Item key="/"><Link href="/">Dashboard</Link></Menu.Item>
+          <Menu.Item key="/issues"><Link href="/issues">Issues</Link></Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ backgroundColor: 'white', padding: "50px 50px" }}>
+        <div className="site-layout-content"><Component {...pageProps} /></div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        TODO
+      </Footer>
+    </Layout>
+  )
 }
 export default MyApp
---
+++ pages/issues/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Issues() {
+  return (
+    <div>
+      Issues
+    </div>
+  )
+}
