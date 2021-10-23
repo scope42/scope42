b00e611a17559c170a7325b5c6d965d2107d624b
Erik Hofer <erik-hofer@mail.de>
2021-10-23 15:07:45 +0200

    Add nprogress

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -5,6 +5,20 @@ import { Layout, Menu } from 'antd'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const { Header, Content, Footer } = Layout
 
