d9756fb5401cbc19394c7fa851c0397232d6e89a
Erik Hofer <erik-hofer@mail.de>
2021-12-07 11:24:44 +0100

    Add single issue page

--- ui/pages/issues/[id].tsx
+++ ui/pages/issues/[id].tsx
@@ -2,9 +2,27 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { Issue, loadIssue } from '../../data'
 import ReactMarkdown from 'react-markdown/react-markdown.min'
+import { PageHeader, Tag, Row, Descriptions, Badge } from 'antd'
 
 const IssuePage = ({ issue }: { issue: Issue }) => {
-  return <><p>Issue: {issue.title}</p>
+  return <>
+    <PageHeader
+      onBack={() => window.history.back()}
+      title={<><Badge color={'cyan'} />{issue.title}</>}
+      subTitle="This is a subtitle"
+    >
+      <Row>
+        <Descriptions size="small" column={3}>
+          <Descriptions.Item label="Status">
+            <Tag color="green">Open</Tag>
+          </Descriptions.Item>
+          <Descriptions.Item label="Created">2017-01-10</Descriptions.Item>
+          <Descriptions.Item label="Tags">
+            <Tag>Frontend</Tag>
+          </Descriptions.Item>
+        </Descriptions>
+      </Row>
+    </PageHeader>
     <ReactMarkdown>{issue.body}</ReactMarkdown>
   </>
 }
