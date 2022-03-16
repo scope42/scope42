f957e5561bd51f91c97129df83aab7c1a1471a5c
Erik Hofer <erik-hofer@mail.de>
2022-03-16 23:28:54 +0100

    feat: improve external link icon

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,5 +1,22 @@
-import { LinkOutlined } from '@ant-design/icons'
+import Icon from '@ant-design/icons'
 import React from 'react'
+
+/** Adapted from https://commons.wikimedia.org/wiki/File:External.svg (public domain) */
+const IconSvg = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 100 100"
+  >
+    <path
+      fill="#FFF"
+      stroke="#06D"
+      stroke-width="6"
+      d="m43,35H5v60h60V57M45,5v10l10,10-30,30 20,20 30-30 10,10h10V5z"
+    />
+  </svg>
+)
 
 interface ExternalLinkProps {
   url: string
@@ -9,12 +26,13 @@ interface ExternalLinkProps {
 export const ExternalLink: React.FC<ExternalLinkProps> = props => {
   return (
     <a href={props.url} target="_blank" rel="noopener noreferrer">
+      {props.children}
       {props.noIcon ? null : (
         <>
-          <LinkOutlined />{' '}
+          {' '}
+          <Icon component={IconSvg} />
         </>
       )}
-      {props.children}
     </a>
   )
 }
