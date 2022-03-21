4bfc789cb13c936469c8824b87a56fb391e241a1
Erik Hofer <erik-hofer@mail.de>
2022-03-21 20:14:35 +0100

    feat: implement sorting in item tables

--- src/components/ItemsTable.tsx
+++ src/components/ItemsTable.tsx
@@ -1,22 +1,40 @@
 import { Table, Tag } from 'antd'
+import { ColumnsType } from 'antd/lib/table'
+import dayjs from 'dayjs'
 import React from 'react'
 import { ItemWithId } from '../data/types'
 import { renderDate } from '../data/util'
 import { ItemLink } from './ItemLink'
 import { ItemStatus } from './Status'
 
-const columns = [
+const alphabeticSorter =
+  (extractProperty: (item: ItemWithId) => string | null | undefined) =>
+  (a: ItemWithId, b: ItemWithId) => {
+    const valA = extractProperty(a) || ''
+    const valB = extractProperty(b) || ''
+    return valA.localeCompare(valB)
+  }
+
+const dateSorter =
+  (extractProperty: (item: ItemWithId) => Date | null | undefined) =>
+  (a: ItemWithId, b: ItemWithId) => {
+    return dayjs(extractProperty(a)).diff(extractProperty(b))
+  }
+
+const columns: ColumnsType<ItemWithId> = [
   {
     title: 'Title',
     dataIndex: ['data', 'title'],
     key: 'title',
-    render: (_: string, item: ItemWithId) => <ItemLink id={item.id} />
+    render: (_: string, item: ItemWithId) => <ItemLink id={item.id} />,
+    sorter: alphabeticSorter(item => item.data.title)
   },
   {
     title: 'Status',
     dataIndex: ['data', 'status'],
     key: 'status',
-    render: (_: any, item: ItemWithId) => <ItemStatus item={item} />
+    render: (_: any, item: ItemWithId) => <ItemStatus item={item} />,
+    sorter: alphabeticSorter(item => item.data.status)
   },
   {
     title: 'Tags',
@@ -28,13 +46,16 @@ const columns = [
     title: 'Created',
     dataIndex: ['data', 'created'],
     key: 'created',
-    render: renderDate
+    render: renderDate,
+    sorter: dateSorter(item => item.data.created)
   },
   {
     title: 'Modified',
     dataIndex: ['data', 'modified'],
     key: 'modified',
-    render: renderDate
+    render: renderDate,
+    sorter: dateSorter(item => item.data.modified),
+    defaultSortOrder: 'descend'
   }
 ]
 
