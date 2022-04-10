3674bdd33d0fa2b380f05d37540a1d26182e7d13
Erik Hofer <erik-hofer@mail.de>
2022-04-10 19:41:42 +0200

    feat: item links in markdown

--- src/features/markdown/Markdown.tsx
+++ src/features/markdown/Markdown.tsx
@@ -1,16 +1,41 @@
-import ReactMarkdown from 'react-markdown'
-import { ReactMarkdownOptions } from 'react-markdown/lib/react-markdown'
+import ReactMarkdown, { Components } from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { ExternalLink } from '../../components/ExternalLink'
 import styles from './Markdown.module.css'
+import { Plugin } from 'unified'
+import { Root } from 'mdast'
+import { visit } from 'unist-util-visit'
+import remarkDirective from 'remark-directive'
+import { ItemLink } from '../../components/ItemLink'
 
-const COMPONENTS: ReactMarkdownOptions['components'] = {
+const remarkItemLink: Plugin<[], Root> = () => {
+  return tree => {
+    visit(tree, node => {
+      if (node.type === 'textDirective' && node.name === 'link') {
+        const data = node.data || (node.data = {})
+        const attributes = node.attributes || {}
+        const itemId = attributes.id
+
+        if (!itemId) return
+
+        data.hName = 'scope42-item-link'
+        data.hProperties = { 'item-id': itemId }
+      }
+    })
+  }
+}
+
+const COMPONENTS: Components = {
   a({ node, children, href, ...restProps }) {
     return href ? (
       <ExternalLink url={href}>{children}</ExternalLink>
     ) : (
       <a {...restProps}>{children}</a>
     )
+  },
+  // @ts-ignore
+  'scope42-item-link'(props) {
+    return <ItemLink id={props['item-id']} />
   }
 }
 
@@ -18,7 +43,7 @@ export const Markdown: React.FC<{ children: string }> = props => {
   return (
     <ReactMarkdown
       className={styles.markdown}
-      remarkPlugins={[remarkGfm]}
+      remarkPlugins={[remarkGfm, remarkDirective, remarkItemLink]}
       components={COMPONENTS}
     >
       {props.children}
