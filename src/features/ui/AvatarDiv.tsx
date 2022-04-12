aa68aaa0543fdc1700ffcc263df606ba81ad17b1
Erik Hofer <erik-hofer@mail.de>
2022-04-12 16:22:05 +0200

    fix: AvatarDiv

--- src/features/ui/AvatarDiv.tsx
+++ src/features/ui/AvatarDiv.tsx
@@ -10,13 +10,15 @@ export const AvatarDiv: React.FC<AvatarDivProps> = props => {
   const { avatar, tooltip, style, ...restProps } = props
   return (
     <div style={{ display: 'flex', gap: 8, ...style }} {...restProps}>
-      {props.tooltip ? (
-        <Tooltip title={props.tooltip}>
-          <div style={{ cursor: 'help' }}>{props.avatar}</div>
-        </Tooltip>
-      ) : (
-        props.avatar
-      )}
+      <div style={{ flexShrink: 0 }}>
+        {props.tooltip ? (
+          <Tooltip title={props.tooltip}>
+            <div style={{ cursor: 'help' }}>{props.avatar}</div>
+          </Tooltip>
+        ) : (
+          props.avatar
+        )}
+      </div>
       <div>{props.children}</div>
     </div>
   )
