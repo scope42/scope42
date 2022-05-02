9a39dca571564a5e2464dda8bd8a4b3402fa462d
Erik Hofer <erik-hofer@mail.de>
2022-05-02 12:19:12 +0200

    feat: switch from topnav to sidenav (#90)
    * refactor: use actual icon component for item icons
    
    * feat: switch from topnav to sidenav

--- src/features/layout/AddItemButton.tsx
+++ src/features/layout/AddItemButton.tsx
@@ -1,8 +1,13 @@
 import { PlusOutlined } from '@ant-design/icons'
 import { Dropdown, Menu } from 'antd'
 import Button from 'antd-button-color'
-import { useEditorStore } from './ItemEditor/ItemEditor'
-import { DecisionIcon, ImprovementIcon, IssueIcon, RiskIcon } from './ItemIcon'
+import { useEditorStore } from '../../components/ItemEditor/ItemEditor'
+import {
+  DecisionIcon,
+  ImprovementIcon,
+  IssueIcon,
+  RiskIcon
+} from '../../components/ItemIcon'
 
 export const AddItemButton: React.FC = () => {
   const editor = useEditorStore(
@@ -16,25 +21,33 @@ export const AddItemButton: React.FC = () => {
 
   const addMenu = (
     <Menu>
-      <Menu.Item key="issue" icon={<IssueIcon />} onClick={editor.createIssue}>
-        <span style={{ marginLeft: 8 }}>Issue</span>
+      <Menu.Item
+        key="issue"
+        icon={<IssueIcon style={{ fontSize: '20px' }} />}
+        onClick={editor.createIssue}
+      >
+        Issue
       </Menu.Item>
       <Menu.Item
         key="improvement"
-        icon={<ImprovementIcon />}
+        icon={<ImprovementIcon style={{ fontSize: '20px' }} />}
         onClick={editor.createImprovement}
       >
-        <span style={{ marginLeft: 8 }}>Improvement</span>
+        Improvement
       </Menu.Item>
-      <Menu.Item key="risk" icon={<RiskIcon />} onClick={editor.createRisk}>
-        <span style={{ marginLeft: 8 }}>Risk</span>
+      <Menu.Item
+        key="risk"
+        icon={<RiskIcon style={{ fontSize: '20px' }} />}
+        onClick={editor.createRisk}
+      >
+        Risk
       </Menu.Item>
       <Menu.Item
         key="decision"
-        icon={<DecisionIcon />}
+        icon={<DecisionIcon style={{ fontSize: '20px' }} />}
         onClick={editor.createDecision}
       >
-        <span style={{ marginLeft: 8 }}>Decision</span>
+        Decision
       </Menu.Item>
     </Menu>
   )
---
+++ src/features/layout/SideNav.tsx
@@ -0,0 +1,63 @@
+import { HomeOutlined, SearchOutlined } from '@ant-design/icons'
+import { Menu, Layout } from 'antd'
+import { Link, useLocation } from 'react-router-dom'
+import {
+  DecisionIcon,
+  ImprovementIcon,
+  IssueIcon,
+  RiskIcon
+} from '../../components/ItemIcon'
+
+export const SideNav: React.VFC = () => {
+  const location = useLocation()
+
+  const basePath = '/' + location.pathname.split('/')[1]
+  return (
+    <Layout.Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        bottom: 0
+      }}
+    >
+      <div
+        style={{
+          height: 64,
+          display: 'flex',
+          alignItems: 'center',
+          paddingLeft: 24
+        }}
+      >
+        <img
+          src={process.env.PUBLIC_URL + '/logo_small_white.svg'}
+          alt="scope42"
+          width={48.005852 * 3}
+          height={9.3873901 * 3}
+        />
+      </div>
+      <Menu theme="dark" mode="inline" selectedKeys={[basePath]}>
+        <Menu.Item key="/" icon={<HomeOutlined />}>
+          <Link to="/">Introduction</Link>
+        </Menu.Item>
+        <Menu.Item key="/issues" icon={<IssueIcon />}>
+          <Link to="/issues">Issues</Link>
+        </Menu.Item>
+        <Menu.Item key="/improvements" icon={<ImprovementIcon />}>
+          <Link to="/improvements">Improvements</Link>
+        </Menu.Item>
+        <Menu.Item key="/risks" icon={<RiskIcon />}>
+          <Link to="/risks">Risks</Link>
+        </Menu.Item>
+        <Menu.Item key="/decisions" icon={<DecisionIcon />}>
+          <Link to="/decisions">Decisions</Link>
+        </Menu.Item>
+        <Menu.Item key="/search" icon={<SearchOutlined />}>
+          <Link to="/search">Search</Link>
+        </Menu.Item>
+      </Menu>
+    </Layout.Sider>
+  )
+}
