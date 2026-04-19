c5d82929d41df54c77702b847a5cac177f49d465
Erik Hofer <git@erikhofer.de>
2026-04-19 20:46:22 +0200

    chore(examples): point data-processing at new example/ workspace

--- examples/data-processing/src/index.ts
+++ examples/data-processing/src/index.ts
@@ -1,6 +1,6 @@
 import { Workspace } from '@scope42/data'
 import { NodeDirectoryHandle } from '@scope42/data/dist/io/adapters/node'
 
-const workspace = new Workspace(new NodeDirectoryHandle('../../app/example'))
+const workspace = new Workspace(new NodeDirectoryHandle('../../example'))
 
 workspace.readItems().then(console.log)
